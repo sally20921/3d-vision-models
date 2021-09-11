228dc7d352a0b078b2fff8b34fdabe0ddfdbc0f2
sally20921 <sally20921@github.com>
2021-09-11 01:50:13 +0000

    slow server

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 ## Dealing with KITTI dataset
 - the KITTI rig has a baseline of 54cm.
-- to convert our stereo predictions to real-world scale, we multiply our depths by `STEREO_SCALE_FACTOR = 5.4` 
+- to convert our stereo predictions to real-world scale, we multiply our depths by `STEREO_SCALE_FACTOR = 5.4`
+
+## Dealing with various image formats
+
+* PIL: RGB image [W,H,C]
+* cv2: BGR image [H,W,C] returns numpy.array
+* numpy: BGR image [H,W,C]
+* pytorch: RGB image [B,C,H,W]
+
--- packnet-sfm/utils/image.py
+++ packnet-sfm/utils/image.py
@@ -34,8 +34,9 @@ def write_image(filename, image):
         cv2.imfunc(bgr)
         # converts to 8-bit unsigned (CV_8U) and saved 
         # only 8-bit single channel or 3-channel ('bgr') allowed
-
-    ex. PIL reads the RGB image [W,H]
+        
+    ex. regular file is RGB image
+        PIL reads the RGB image [W,H,C]
         cv2 reads the BGR image [H,W,C] to a numpy array
         # to view cv2 image, change BGR -> RGB
         numpy BGR image convention [H,W,C]
@@ -61,3 +62,37 @@ def flip_lr(image):
     image_flipped: torch.Tensor [B,3,H,W]
         flipped image
     '''
+    assert image.dim() == 4, "Please provide [B,C,H,W] image to flip horizontally"
+    return torch.flip(image, [3])
+    # changed the ordering of the width 
+
+def flip_model(model, image, flip):
+    '''
+    if flip is true:
+        flip input image and then processes model(input) 
+        then flip the output again => flip(model(flip(input)))
+    else:
+        just model(input)
+
+    Parameters
+    ______
+    model: nn.Module
+        nn.Module to be used 
+    image: torch.Tensor [B,3,H,W]
+        input image
+    flip: bool
+        true if the flip is happening
+    
+    Returns
+    ______
+    inv_depths: list of torch.Tensor [B,1,H,W]
+        list of predicted inverse depth maps
+    '''
+    if flip:
+        return [flip_lr(inv_depth) for inv_depth in model(flip_lr(image))]
+    else:
+        return model(image)
+
+
+
+
