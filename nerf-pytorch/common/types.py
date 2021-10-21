f7405dee3a0099090320a555f2df69f502fd3480
sally20921 <sally20921@github.com>
2021-10-21 08:51:06 +0000

    working on common

--- nerf-pytorch/common/types.py
+++ nerf-pytorch/common/types.py
@@ -5,4 +5,19 @@ import torch
 Device = Union[str, torch.device]
 
 def make_device(device: Device) -> torch.device:
+    '''
+    Makes an actual torch.device object from the device
+    specified as either a string or torch.device object.
+    If the device is `cuda` without a specific index, 
+    the index of the current device is assigned.
+
+    Args
+    ____
+        device: Device (as str or torch.device)
+    
+    Returns
+    _____
+        a matching torch.device object 
+    '''
+
     
